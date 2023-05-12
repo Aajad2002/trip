6c359a4cd861f42d7975e787474044c1da4a1a7e
Aajad2002 <aajadprajapat28@gmail.com>
2023-05-12 23:32:40 +0530

    trip

--- routes/User.Routes.js
+++ routes/User.Routes.js
@@ -14,12 +14,12 @@ userRouter.post("/register", async (req, res) => {
         let user = await UserModel.findOne({ email })
 
         if (user) {
-            res.status(200).send("User already exists on this email" )
+            res.status(200).send({"msg":"User already exists on this email"} )
         } else {
             bcrypt.hash(password, 6, async function (err, hash) {
                 const user = new UserModel({ ...req.body, password: hash })
                 await user.save()
-                res.status(200).send( "Registration Successfull,Login Now!!" )
+                res.status(200).send({"msg": "Registration Successfull,Login Now!!" })
             });
         }
     } catch (error) {
@@ -37,11 +37,11 @@ userRouter.post("/login", async (req, res) => {
         let user = await UserModel.findOne({ email })
 
         if (!user) {
-            res.status(200).send("User doesn't exits on this email" )
+            res.status(200).send({"msg":"User doesn't exits on this email"} )
         } else {
             bcrypt.compare(password, user.password,async function(err, result) {
                 if(result){
-                    res.status(200).send("Login successfull")
+                    res.status(200).send({"msg":"Login successfull",name:user.name})
                 }else{
                     res.status(201).send("Wrong credentials")
                 }
